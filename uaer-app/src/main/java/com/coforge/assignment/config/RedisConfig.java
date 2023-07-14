67d8a4d12e51754f85ca882d617596205d2aba78
sunil965 <sunilkumar192114@gmail.com>
2023-07-14 19:02:30 +0530

    Redis Implementation.

---
+++ uaer-app/src/main/java/com/coforge/assignment/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.coforge.assignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(5))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+    }
+
+    // Below code is to initialize RedisCacheManagerBuilderCustomizer bean to have full control over the caching setup.
+    // This helps to further fine-tune the caching behavior on a per-cache basis including null values, key prefixes and
+    // binary serialization.
+/*
+    @Bean
+    public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer() {
+        return (builder) -> builder
+                .withCacheConfiguration("itemCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(10)))
+                .withCacheConfiguration("customerCache",
+                        RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofMinutes(5)));
+    }
+ */
+}
